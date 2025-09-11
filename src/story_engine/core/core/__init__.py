bc31ca368f832ae681ccb3a6ce2882a1641fe83b
samscarrow <93792006+samscarrow@users.noreply.github.com>
2025-09-11 01:32:05 -0400

    pkg: add __init__.py to src/story_engine/core/core to stabilize imports for services (Message/SCENE_DONE usage)

---
+++ src/story_engine/core/core/__init__.py
@@ -0,0 +1,7 @@
+"""
+Core subpackages for story engine (cache, messaging, orchestration, story_engine, etc.).
+
+Adding this file ensures reliable package imports in environments that do not
+fully support implicit namespace packages.
+"""
+
